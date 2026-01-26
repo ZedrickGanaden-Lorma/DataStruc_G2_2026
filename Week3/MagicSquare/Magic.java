270c3e477eb687e4331b3c72fb156425e96772f8
unknown <zedrick.ganaden@lorma.edu>
2026-01-26 12:40:12 +0800

    fix folder structure

--- Week3/MagicSquare/Magic.java
+++ Week3/MagicSquare/Magic.java
@@ -16,31 +16,6 @@ public class Magic {
         int y = ay - 1;
         int x = (int) ax / 2;
         for (int a = 1; true; a++) {
-            int jumps = 0;
-            System.out.println("Overwriting : " + arr[y % ax][x % ay]);
-            boolean end = false;
-            while (true) {
-                if (arr[y % ay][x % ax] != 0) {
-                    jumps += 1;
-                    y += 2;
-                    x -= 1;
-                    if (jumps > 2) {
-                        System.out.println("Exiting");
-                        end = true;
-                        break;
-                    }
-                } else {
-                    arr[y % ay][x % ax] = a;
-                    break;
-                }
-            }
-            if (end)
-                break;
-            System.out.println("y : " + y + " x : " + x);
-            System.out.println("y : " + y % ay + " x : " + x % ax);
-            printArr(arr);
-            y++;
-            x++;
         }
     }
 }
